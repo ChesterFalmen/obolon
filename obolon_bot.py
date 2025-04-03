35dcb08a50b0b4329409a2eef7d1e9a35435487a
ChesterFalmen <yrchenko.vas@i.ua>
2025-04-03 10:00:29 +0300

    + letterrr

--- obolon_bot.py
+++ obolon_bot.py
@@ -185,7 +185,7 @@ def fetch_data(session):
 # === Головний цикл ===
 if __name__ == "__main__":
     session = login()
-    print("Останнє оновлення 02.04 12:14")
+    print("Останнє оновлення 03.04 10:00")
     while True:
         if session:
             result = fetch_data(session)
